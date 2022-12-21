11a2fcaa7c2b7ada25de5b5259cd36c2c831fe8f
sparky23172 <46407811+sparky23172@users.noreply.github.com>
2022-12-21 03:03:01 -0600

    Update KeyInsert.py
     - Made some edits to the text shown
     - Started working on having the countdown show on the GUI as well as the command-line

--- KeyInsert.py
+++ KeyInsert.py
@@ -5,70 +5,77 @@ from tkinter import scrolledtext
 import time
 
 
-def writez(text):
+TITLE="KeyInsert"
+COUNTER = "Click Submit to start countdown"
+
+def writez(text, targetFrame):
     print(text)
     timer = 3
     count = timer
     while count != 0:
+        if targetFrame != None:
+            targetFrame.config(text="{}".format(count))
         print("Countdown: {}".format(count))
         time.sleep(1)
         count -= 1
     auto.typewrite(text, interval=0.1)
     print(text, "has been written")
+    if targetFrame != None:
+        targetFrame.config(text="Text has been written")
 
 
 def main():
 
-    resp = auto.confirm("What would you like to paste?\n[*] They are both funtionally the same. Just the multiline is nicer on the eyes", buttons=['Single Line', 'Multi Line',])
-
+    resp = auto.confirm("What would you like to paste?\n[*] They are both funtionally the same. Just the multiline is nicer on the eyes", buttons=['Single Line', 'Multi Line',], title=TITLE)
     if resp == "Multi Line":
         root = tk.Tk()
+        root.title(TITLE)
+
         frame_a = tk.Frame()
         entry = tk.Entry(master=frame_a, width=50)
 
-        label_a = tk.Label(master=frame_a, text="Hey! This is the Single lined KeyLogger Program!\n\nPlease insert the text you want to paste below\n\n")
+        label_a = tk.Label(master=frame_a, text="Hey! This is the Multi lined KeyLogger Program!\n\nPlease insert the text you want to paste below\n\n")
         label_a.pack()
 
         entry.pack()
-  
-        root.title("ScrolledText Widget Example")
         
         ttk.Label(root, text="Hey! This is the Single lined KeyLogger Program!\n\nPlease insert the text you want to paste below\n\n",
                 font=("Times New Roman", 15)).grid(column=0, row=0)
-        ttk.Label(root, text="Text to Paste:", font=("Bold", 12)).grid(column=0, row=1)
+        ttk.Label(root, text=COUNTER, font=("Bold", 12)).grid(column=0, row=1)
+        ttk.Label(root, text="Text to Paste:", font=("Bold", 12)).grid(column=0, row=2)
         ttk.Label(frame_a, text="Test")
         
         text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD,
                                             width=40, height=8,
                                             font=("Times New Roman", 15))
         
-        sub_btn=tk.Button(root,text = 'Submit', command = lambda: writez(text_area.get("1.0", "end-1c")))
+        sub_btn=tk.Button(root,text = 'Submit', command = lambda: writez(text_area.get("1.0", "end-1c"), None))
         text_area.grid(column=0, row=2, pady=10, padx=10)
         sub_btn.grid(column=0, row=3, pady=10, padx=10)
 
-        
         # placing cursor in text area
         text_area.focus()
         root.mainloop()
 
     if resp == "Single Line":
         window = tk.Tk()
-
+        window.title(TITLE)
+        
         frame_a = tk.Frame()
-        entry = tk.Entry(master=frame_a, width=50)
-
-        label_a = tk.Label(master=frame_a, text="Hey! This is the Single lined KeyLogger Program!\n\nPlease insert the text you want to paste below\n\n")
-        label_a.pack()
-
-        entry.pack()
-
         frame_b = tk.Frame()
-        button = tk.Button(master=frame_b, text="Paste Text", command=lambda: writez(entry.get()))
-        button.pack()
 
+        entry = tk.Entry(master=frame_a, width=50)
+        label_a = tk.Label(master=frame_a, text="Hey! This is the Single lined KeyLogger Program!\n\nPlease insert the text you want to paste below\n")
+        frameC = tk.Label(master=frame_a, text=COUNTER)
+        button = tk.Button(master=frame_b, text="Submit", command=lambda: writez(entry.get(), frameC))
+
+        label_a.pack()
+        frameC.pack()
+        entry.pack()
+        button.pack()
         frame_a.pack()
         frame_b.pack()
-
+        
         window.mainloop()
 
         
